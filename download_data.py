c4f9b2c312fe9ab5d25376375851dc7535829c22
Jorge José Vega Espinar <jor0veg@gmail.com>
2023-04-15 16:26:16 +0200

    mapa de ficheros - activos

--- download_data.py
+++ download_data.py
@@ -1,12 +1,17 @@
 #%%
 import streamlit as st
 from os import listdir
+import pandas as pd
 #%%
-st.title("Descargar datos", )
-st.markdown("Selecciona los datos a descargar")
 files = listdir(".")
 files = [f for f in files if f.endswith(".csv")]
 files = sorted(files)
+activos = ['AUDUSD', 'GBPUSD', 'EURUSD', 'Brent', 'Bitcoin', 'Dollar Index', 'S&P500', 'DAX Futuro','DAX Mini', 'Gold', 'Crudo', 'Nasdaq', 'News', 'Silver', 'Dow']
+# Mapa de nombres - activos
+st.write("## Mapa de nombres - activos")
+st.table(pd.DataFrame({"Archivo": files, "Activo": activos}))
+st.title("Descargar datos")
+st.markdown("Selecciona los datos a descargar")
 file = st.selectbox("Archivo", files)
 st.download_button(label="Descargar", data=open(file, "rb").read(), file_name=file, mime="text/csv")
 
