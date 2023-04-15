40b52b91967c968d8344bce1234a4620ab386c10
Jorge José Vega Espinar <jor0veg@gmail.com>
2023-04-15 14:51:38 +0200

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -2,18 +2,12 @@
 import streamlit as st
 from os import listdir
 #%%
-# Crear boton desplegable para seleccionar el archivo a descargar
 st.title("Descargar datos", )
-st.markdown("Selecciona el archivo a descargar")
+st.markdown("Selecciona los datos a descargar")
 files = listdir(".")
-# Solo los que acaban en .csv
 files = [f for f in files if f.endswith(".csv")]
-# Ordenar alfabeticamente
 files = sorted(files)
-#st.text(files)
-# Crear boton desplegable
 file = st.selectbox("Archivo", files)
-# Crear boton para descargar
 st.download_button(label="Descargar", data=open(file, "rb").read(), file_name=file, mime="text/csv")
 
 # %%
