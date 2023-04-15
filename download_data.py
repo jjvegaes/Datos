8ac8e119a6c14ba35f2aafe99bbed9f4629fdaa9
Jorge José Vega Espinar <jor0veg@gmail.com>
2023-04-15 16:31:00 +0200

    .

--- download_data.py
+++ download_data.py
@@ -9,6 +9,7 @@ files = sorted(files)
 activos = ['AUDUSD', 'GBPUSD', 'EURUSD', 'Brent', 'Bitcoin', 'Dollar Index', 'S&P500', 'DAX Futuro','DAX Mini', 'Gold', 'Crudo', 'Nasdaq', 'News', 'Silver', 'Dow']
 # Mapa de nombres - activos
 st.write("## Mapa de nombres - activos")
+st.markdown("Última actualización: 15-04-2023")
 st.table(pd.DataFrame({"Archivo": files, "Activo": activos}))
 st.title("Descargar datos")
 st.markdown("Selecciona los datos a descargar")
