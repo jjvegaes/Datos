6253f8ec7f5bfb06f70b11465e8e22a380505faa
Jorge José Vega Espinar <jor0veg@gmail.com>
2023-04-15 14:42:06 +0200

    Create download_data.py

---
+++ download_data.py
@@ -0,0 +1,17 @@
+#%%
+import streamlit as st
+from os import listdir
+#%%
+# Crear boton desplegable para seleccionar el archivo a descargar
+st.title("Descargar datos", )
+st.markdown("Selecciona el archivo a descargar")
+files = listdir(".")
+# Solo los que acaban en .csv
+files = [f for f in files if f.endswith(".csv")]
+#st.text(files)
+# Crear boton desplegable
+file = st.selectbox("Archivo", files)
+# Crear boton para descargar
+st.download_button(label="Descargar", data=open(file, "rb").read(), file_name=file, mime="text/csv")
+
+# %%
